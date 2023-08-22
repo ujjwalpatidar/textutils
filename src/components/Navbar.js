ff12838ec806af85cae036d4eb3fefe6ad3cbb04
ujjwalpatidar <ujjwalpatidar88@gmail.com>
2023-08-22 15:19:04 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,7 +35,7 @@ export default function Navbar(props) {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <Link className="nav-link active" aria-current="page" to="/">
-                Home | Home
+                Home 
               </Link>
             </li>
             <li className="nav-item">
